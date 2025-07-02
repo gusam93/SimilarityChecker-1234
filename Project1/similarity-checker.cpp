fcfd2c188075ea0bbaa58f6ab80af68a9aea59cf
hyeongseok.choi <babmukja0322@gmail.com>
2025-07-02 17:04:39 +0900

    코드 리팩터링

--- Project1/similarity-checker.cpp
+++ Project1/similarity-checker.cpp
@@ -8,40 +8,60 @@ using std::set;
 class SimlilarityChecker {
 public:
     int getResult(const string& string1, const string& string2) {
-        if (string1 == string2) return 100;
-        double lengthScore = getLengthScore(string1.size(), string2.size());
-        double sameCharScore = getSameCharScore(string1, string2);
-        return lengthScore + sameCharScore;
+        if (isSameText(string1, string2)) return PERFECT_SCORE;
+        return getLengthScore(string1, string2) + getSameCharScore(string1, string2);
     }
 
-    double getLengthScore(int string1Length, int string2Length)
+    bool isSameText(const std::string& string1, const std::string& string2)
     {
-        if (string1Length == string2Length) return 60;
-        const double longLength = std::max(string1Length, string2Length);
-        const double shortLength = std::min(string1Length, string2Length);
+        return string1 == string2;
+    }
+
+    double getLengthScore(const string& string1, const string& string2)
+    {
+        if (isSameLength(string1, string2)) return MAX_SAME_LENGTH_SCORE;
+        const double longLength = std::max(string1.length(), string2.length());
+        const double shortLength = std::min(string1.length(), string2.length());
 
         if (shortLength * 2 <= longLength) return 0;
-        return (1 - (longLength - shortLength) / shortLength) * 60;
+        double rate = 1 - (longLength - shortLength) / shortLength;
+        return rate * MAX_SAME_LENGTH_SCORE;
+    }
+    bool isSameLength(const std::string& string1, const std::string& string2)
+    {
+        return string1.length() == string2.length();
     }
     double getSameCharScore(const string& string1, const string& string2)
     {
         // 1. 대문자 예외처리 필요
-        std::set<char> totalAlapha;
-        std::set<char> sameAlpha;
+        int totalCnt = getTotalAlphaCnt(string1, string2);
+        int sameCnt = getSameAlphaCnt(string1, string2);
+        auto rate = static_cast<double>(sameCnt / totalCnt);
+        return rate * MAX_SAME_CHAR_SCORE;
+    }
 
+    int getSameAlphaCnt(const std::string& string1,  const std::string& string2)
+    {
+        std::set<char> sameAlpha;
+        for (auto c : string1) {
+            if (string2.find(c) == string::npos) continue;
+            sameAlpha.insert(c);
+        }
+        return sameAlpha.size();
+    }
+    int getTotalAlphaCnt(const std::string& string1, const std::string& string2 )
+    {
+        std::set<char> totalAlapha;
         for (auto ch : string1) {
             totalAlapha.insert(ch);
         }
         for (auto ch : string2) {
             totalAlapha.insert(ch);
         }
-        for (auto c : string1) {
-            if (string2.find(c) == string::npos) continue;
-            sameAlpha.insert(c);
-        }
-
-        if (sameAlpha.size() == 0)return 0;
-
-        return static_cast<double>(sameAlpha.size() / totalAlapha.size()) * 40;
+        return totalAlapha.size();
     }
+  
+    static constexpr int MAX_SAME_LENGTH_SCORE = 60;
+    static constexpr int MAX_SAME_CHAR_SCORE = 40;
+    static constexpr int PERFECT_SCORE = MAX_SAME_CHAR_SCORE + MAX_SAME_LENGTH_SCORE;
 };
