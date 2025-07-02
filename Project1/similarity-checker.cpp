d62604d13df36861e438c8a01c014f10de69e20b
hyeongseok.choi <babmukja0322@gmail.com>
2025-07-02 16:16:14 +0900

    length점수 구현

--- Project1/similarity-checker.cpp
+++ Project1/similarity-checker.cpp
@@ -7,6 +7,16 @@ class SimlilarityChecker {
 public:
     int getResult(const string& string1, const string& string2) {
         if (string1 == string2) return 100;
-        return 0;
+        int lengthScore = getLengthScore(string1.size(), string2.size());
+    }
+
+    int getLengthScore(int string1Length, int string2Length)
+    {
+        if (string1Length == string2Length) return 60;
+        const double longLength = std::max(string1Length, string2Length);
+        const double shortLength = std::min(string1Length, string2Length);
+
+        if ((longLength / shortLength) >= 2) return 0;
+        return (1 - ((longLength - shortLength) / shortLength)) * 60;
     }
 };
--- Project1/similarity-checker_test.cpp
+++ Project1/similarity-checker_test.cpp
@@ -7,6 +7,24 @@ TEST(SimilarityCheckerTest, SameWord)
     SimlilarityChecker checker;
     EXPECT_EQ(100, checker.getResult("ABC", "ABC"));
 }
+TEST(SimilarityCheckerTest, SameLengthWithDifferentWords)
+{
+    SimlilarityChecker checker;
+    EXPECT_EQ(60, checker.getResult("ABC", "DEF"));
+}
+TEST(SimilarityCheckerTest, LengthIsLessEqualWTwice)
+{
+    SimlilarityChecker checker;
+    EXPECT_EQ(20, checker.getResult("ABC", "DEFGE"));
+}
+
+TEST(SimilarityCheckerTest, LengthIsGreaterThanTwice)
+{
+    SimlilarityChecker checker;
+    EXPECT_EQ(0, checker.getResult("ABC", "DDDXFFFF"));
+}
+
+
 
 int main()
 {
